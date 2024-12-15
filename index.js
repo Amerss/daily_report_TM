2d4927471ebd47649416a0b61171330c3fa8bbe5
张震 <13477725227@163.com>
2024-12-15 21:45:15 +0800

    version 0.1.1

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         daily report with aliyun
-// @namespace    http://tampermonkey.net/
-// @version      2024-12-15
+// @namespace    https://github.com/Amerss/daily_report_TM
+// @version      0.1.1
 // @description  云效需求转日报
 // @author       Amerss
 // @match        https://devops.aliyun.com/projex/project/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
-    console.log('hello world')
+    console.log('hello zz')
 })();
